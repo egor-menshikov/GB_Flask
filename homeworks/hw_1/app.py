e3ab7a7659616ac24c89aabfe277865a3d916900
Egor Menshikov <r3born.fs@gmail.com>
2024-03-28 22:22:05 +0300

    hw_1 done

--- homeworks/hw_1/app.py
+++ homeworks/hw_1/app.py
@@ -4,14 +4,19 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/main/')
 def index():
-    return render_template('about.html')
+    return render_template('base_template.html')
 
 
 @app.route('/footwear/')
 def footwear():
     return render_template('footwear.html')
 
+
+@app.route('/clothes/')
+def clothes():
+    return render_template('clothes.html')
 
 if __name__ == '__main__':
     app.run()
